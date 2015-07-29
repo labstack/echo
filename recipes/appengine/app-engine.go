7d928abb0186c9f6f7abd591bbdde9c925c1100f
Simon Green <simon@captaincodeman.com>
2015-07-29 11:30:27 -0600

    gofmt app engine example

--- recipes/appengine/app-engine.go
+++ recipes/appengine/app-engine.go
@@ -3,16 +3,16 @@
 package main
 
 import (
-    "net/http"
-    "github.com/labstack/echo"
+	"github.com/labstack/echo"
+	"net/http"
 )
 
 func createMux() *echo.Echo {
-    e := echo.New()
+	e := echo.New()
 
-    // note: we don't need to provide the middleware or static handlers, that's taken care of by the platform
-    // app engine has it's own "main" wrapper - we just need to hook echo into the default handler
-    http.Handle("/", e)
+	// note: we don't need to provide the middleware or static handlers, that's taken care of by the platform
+	// app engine has it's own "main" wrapper - we just need to hook echo into the default handler
+	http.Handle("/", e)
 
-    return e
+	return e
 }
--- recipes/appengine/app-standalone.go
+++ recipes/appengine/app-standalone.go
@@ -3,23 +3,23 @@
 package main
 
 import (
-    "github.com/labstack/echo"
-    "github.com/labstack/echo/middleware"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func createMux() *echo.Echo {
-    e := echo.New()
+	e := echo.New()
 
-    e.Use(middleware.Recover())
-    e.Use(middleware.Logger())
-    e.Use(middleware.Gzip())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
 
-    e.Index("public/index.html")
-    e.Static("/public", "public")
+	e.Index("public/index.html")
+	e.Static("/public", "public")
 
-    return e
+	return e
 }
 
 func main() {
-    e.Run(":8080")
+	e.Run(":8080")
 }
