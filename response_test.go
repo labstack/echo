51e07af3f0411d4c3b013ce4d4dd0a18231fe49b
Chase Hutchins <chutchins@riotgames.com>
2015-09-13 01:38:22 -0700

    adding code coverage

--- response_test.go
+++ response_test.go
@@ -1,10 +1,11 @@
 package echo
 
 import (
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestResponse(t *testing.T) {
@@ -45,6 +46,9 @@ func TestResponse(t *testing.T) {
 
 	// Size
 	assert.EqualValues(t, len(s), r.Size())
+
+	// Committed
+	assert.Equal(t, true, r.Committed())
 
 	// Hijack
 	assert.Panics(t, func() {
