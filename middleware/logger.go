26ad7e9c1b58fcf31837eaac9ad5754016f282c7
Ben Huson <j.ben.huson@gmail.com>
2015-09-18 11:07:33 -0400

    adding ip address headers back in

--- middleware/logger.go
+++ middleware/logger.go
@@ -13,6 +13,14 @@ func Logger() echo.MiddlewareFunc {
 	return func(h echo.HandlerFunc) echo.HandlerFunc {
 		return func(c *echo.Context) error {
 
+			remoteAddr := c.Request().RemoteAddr
+			if realIP := c.Request().Header.Get("X-Real-IP"); realIP != "" {
+				remoteAddr = realIP
+			}
+			if realIP := c.Request().Header.Get("X-Forwarded-For"); realIP != "" {
+				remoteAddr = realIP
+			}
+
 			start := time.Now()
 			if err := h(c); err != nil {
 				c.Error(err)
@@ -36,7 +44,7 @@ func Logger() echo.MiddlewareFunc {
 				code = color.Cyan(n)
 			}
 
-			remoteAddr, _, _ := net.SplitHostPort(c.Request().RemoteAddr)
+			remoteAddr, _, _ := net.SplitHostPort(remoteAddr)
 
 			log.Printf("%s %s %s %s %s %d", remoteAddr, method, path, code, stop.Sub(start), size)
 			return nil
--- middleware/logger_test.go
+++ middleware/logger_test.go
@@ -9,6 +9,21 @@ import (
 	"github.com/labstack/echo"
 )
 
+func TestRealIPHeader(t *testing.T) {
+	e := echo.New()
+	req, _ := http.NewRequest(echo.GET, "/", nil)
+	req.Header.Add("X-Real-IP", "127.0.0.1")
+	req.Header.Add("X-Forwarded-For", "127.0.0.1")
+	rec := httptest.NewRecorder()
+	c := echo.NewContext(req, echo.NewResponse(rec), e)
+
+	// Status 2xx
+	h := func(c *echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}
+	Logger()(h)(c)
+}
+
 func TestLogger(t *testing.T) {
 	e := echo.New()
 	req, _ := http.NewRequest(echo.GET, "/", nil)
