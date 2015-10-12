5ee38fbfb6c699bd73005da813a9649d09fbccc9
Merten van Gerven <m.vangerven@hotelspecials.nl>
2015-10-12 14:24:23 +0200

    Fixed typo

--- recipes/graceful-shutdown/graceful/server.go
+++ recipes/graceful-shutdown/graceful/server.go
@@ -12,7 +12,7 @@ func main() {
 	// Setup
 	e := echo.New()
 	e.Get("/", func(c *echo.Context) error {
-		return c.String(http.StatusOK, "Sue sews rose on slow jor crows nose")
+		return c.String(http.StatusOK, "Sue sews rose on slow joe crows nose")
 	})
 
 	graceful.ListenAndServe(e.Server(":1323"), 5*time.Second)
