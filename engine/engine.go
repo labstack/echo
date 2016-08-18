0d9c81737191bb43f8e7077688379204548239a9
Vishal Rana <vr@labstack.com>
2016-08-17 20:39:52 -0700

    Fixed godoc for RealIP funtion
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- engine/engine.go
+++ engine/engine.go
@@ -68,7 +68,8 @@ type (
 		// RemoteAddress returns the client's network address.
 		RemoteAddress() string
 
-		// RealIP returns the client's network ip address.
+		// RealIP returns the client's network address based on `X-Forwarded-For`
+		// or `X-Real-IP` request header.
 		RealIP() string
 
 		// Method returns the request's HTTP function.
