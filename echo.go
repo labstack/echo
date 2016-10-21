be7d6962f0eb5a9c06611c97c963698aa70ba52a
Vishal Rana <vr@labstack.com>
2016-10-21 12:49:08 -0700

    fix for #681
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- echo.go
+++ echo.go
@@ -478,11 +478,11 @@ func (e *Echo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c.Reset(r, w)
 
 	// Middleware
-	h := func(Context) error {
+	h := func(c Context) error {
 		method := r.Method
 		path := r.URL.Path
 		e.router.Find(method, path, c)
-		h := c.handler
+		h := c.Handler()
 		for i := len(e.middleware) - 1; i >= 0; i-- {
 			h = e.middleware[i](h)
 		}
