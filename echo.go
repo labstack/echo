a5024b80382f0aa76657fd1ac1b6326411c9afb8
Vishal Rana <vr@labstack.com>
2016-10-24 08:21:36 -0700

    fixed #685
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- echo.go
+++ echo.go
@@ -594,6 +594,7 @@ func WrapMiddleware(m func(http.Handler) http.Handler) MiddlewareFunc {
 	return func(next HandlerFunc) HandlerFunc {
 		return func(c Context) (err error) {
 			m(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				c.SetRequest(r)
 				err = next(c)
 			})).ServeHTTP(c.Response(), c.Request())
 			return
