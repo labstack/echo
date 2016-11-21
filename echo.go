d4dff985faca14344593fcfec5de43ce3d8cc82f
Jonathan Hall <flimzy@flimzy.com>
2016-11-21 17:43:14 +0000

    Minor documentation correction (#742)

--- echo.go
+++ echo.go
@@ -459,7 +459,7 @@ func (e *Echo) Routes() []Route {
 }
 
 // AcquireContext returns an empty `Context` instance from the pool.
-// You must be return the context by calling `ReleaseContext()`.
+// You must return the context by calling `ReleaseContext()`.
 func (e *Echo) AcquireContext() Context {
 	return e.pool.Get().(Context)
 }
