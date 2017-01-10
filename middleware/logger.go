178d37c541513408cb5625b13b8595624fcfed87
Vishal Rana <vr@labstack.com>
2017-01-10 08:40:15 -0800

    Modified logger format
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,18 +1,15 @@
 package middleware
 
 import (
-	"bytes"
 	"io"
 	"os"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
+	"encoding/json"
+
 	"github.com/labstack/echo"
-	"github.com/labstack/gommon/color"
-	isatty "github.com/mattn/go-isatty"
-	"github.com/valyala/fasttemplate"
 )
 
 type (
@@ -21,9 +18,9 @@ type (
 		// Skipper defines a function to skip middleware.
 		Skipper Skipper
 
-		// Log format which can be constructed using the following tags:
+		// Availabe logger fields:
 		//
-		// - time_rfc3339
+		// - time (Unix time)
 		// - id (Request ID - Not implemented)
 		// - remote_ip
 		// - uri
@@ -33,26 +30,40 @@ type (
 		// - referer
 		// - user_agent
 		// - status
-		// - latency (In microseconds)
+		// - latency (In nanosecond)
 		// - latency_human (Human readable)
 		// - bytes_in (Bytes received)
 		// - bytes_out (Bytes sent)
 		// - header:<name>
 		// - query:<name>
 		// - form:<name>
-		//
-		// Example "${remote_ip} ${status}"
-		//
-		// Optional. Default value DefaultLoggerConfig.Format.
-		Format string `json:"format"`
+
+		// Optional. Default value DefaultLoggerConfig.Fields.
+		Fields []string `json:"fields"`
 
 		// Output is a writer where logs are written.
 		// Optional. Default value os.Stdout.
 		Output io.Writer
+	}
 
-		template *fasttemplate.Template
-		color    *color.Color
-		pool     sync.Pool
+	loggerFields struct {
+		// ID string `json:"id,omitempty"` (Request ID - Not implemented)
+		Time         int64             `json:"time,omitempty"`
+		RemoteIP     string            `json:"remote_ip,omitempty"`
+		URI          string            `json:"uri,omitempty"`
+		Host         string            `json:"host,omitempty"`
+		Method       string            `json:"method,omitempty"`
+		Path         string            `json:"path,omitempty"`
+		Referer      string            `json:"referer,omitempty"`
+		UserAgent    string            `json:"user_agent,omitempty"`
+		Status       int               `json:"status,omitempty"`
+		Latency      time.Duration     `json:"latency,omitempty"`
+		LatencyHuman string            `json:"latency_human,omitempty"`
+		BytesIn      int64             `json:"bytes_in,omitempty"`
+		BytesOut     int64             `json:"bytes_out,omitempty"`
+		Header       map[string]string `json:"header,omitempty"`
+		Form         map[string]string `json:"form,omitempty"`
+		Query        map[string]string `json:"query,omitempty"`
 	}
 )
 
@@ -60,12 +71,19 @@ var (
 	// DefaultLoggerConfig is the default Logger middleware config.
 	DefaultLoggerConfig = LoggerConfig{
 		Skipper: defaultSkipper,
-		Format: `{"time":"${time_rfc3339}","remote_ip":"${remote_ip}","host":"${host}",` +
-			`"method":"${method}","uri":"${uri}","status":${status}, "latency":${latency},` +
-			`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
-			`"bytes_out":${bytes_out}}` + "\n",
+		Fields: []string{
+			"time",
+			"remote_ip",
+			"host",
+			"method",
+			"uri",
+			"status",
+			"latency",
+			"latency_human",
+			"bytes_in",
+			"bytes_out",
+		},
 		Output: os.Stdout,
-		color:  color.New(),
 	}
 )
 
@@ -81,22 +99,11 @@ func LoggerWithConfig(config LoggerConfig) echo.MiddlewareFunc {
 	if config.Skipper == nil {
 		config.Skipper = DefaultLoggerConfig.Skipper
 	}
-	if config.Format == "" {
-		config.Format = DefaultLoggerConfig.Format
+	if len(config.Fields) == 0 {
+		config.Fields = DefaultLoggerConfig.Fields
 	}
 	if config.Output == nil {
 		config.Output = DefaultLoggerConfig.Output
-	}
-
-	config.template = fasttemplate.New(config.Format, "${", "}")
-	config.color = color.New()
-	if w, ok := config.Output.(*os.File); !ok || !isatty.IsTerminal(w.Fd()) {
-		config.color.Disable()
-	}
-	config.pool = sync.Pool{
-		New: func() interface{} {
-			return bytes.NewBuffer(make([]byte, 256))
-		},
 	}
 
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
@@ -112,74 +119,78 @@ func LoggerWithConfig(config LoggerConfig) echo.MiddlewareFunc {
 				c.Error(err)
 			}
 			stop := time.Now()
-			buf := config.pool.Get().(*bytes.Buffer)
-			buf.Reset()
-			defer config.pool.Put(buf)
+			fields := &loggerFields{
+				Header: make(map[string]string),
+				Form:   make(map[string]string),
+				Query:  make(map[string]string),
+			}
 
-			_, err = config.template.ExecuteFunc(buf, func(w io.Writer, tag string) (int, error) {
-				switch tag {
-				case "time_rfc3339":
-					return w.Write([]byte(time.Now().Format(time.RFC3339)))
+			for _, f := range config.Fields {
+				switch f {
+				case "time":
+					fields.Time = time.Now().Unix()
 				case "remote_ip":
-					ra := c.RealIP()
-					return w.Write([]byte(ra))
+					fields.RemoteIP = c.RealIP()
 				case "host":
-					return w.Write([]byte(req.Host))
+					fields.Host = req.Host
 				case "uri":
-					return w.Write([]byte(req.RequestURI))
+					fields.URI = req.RequestURI
 				case "method":
-					return w.Write([]byte(req.Method))
+					fields.Method = req.Method
 				case "path":
 					p := req.URL.Path
 					if p == "" {
 						p = "/"
 					}
-					return w.Write([]byte(p))
+					fields.Path = p
 				case "referer":
-					return w.Write([]byte(req.Referer()))
+					fields.Referer = req.Referer()
 				case "user_agent":
-					return w.Write([]byte(req.UserAgent()))
+					fields.UserAgent = req.UserAgent()
 				case "status":
-					n := res.Status
-					s := config.color.Green(n)
-					switch {
-					case n >= 500:
-						s = config.color.Red(n)
-					case n >= 400:
-						s = config.color.Yellow(n)
-					case n >= 300:
-						s = config.color.Cyan(n)
-					}
-					return w.Write([]byte(s))
+					// n := res.Status
+					// s := config.color.Green(n)
+					// switch {
+					// case n >= 500:
+					// 	s = config.color.Red(n)
+					// case n >= 400:
+					// 	s = config.color.Yellow(n)
+					// case n >= 300:
+					// 	s = config.color.Cyan(n)
+					// }
+					// return w.Write([]byte(s))
+					fields.Status = res.Status
 				case "latency":
-					l := stop.Sub(start).Nanoseconds() / 1000
-					return w.Write([]byte(strconv.FormatInt(l, 10)))
+					fields.Latency = stop.Sub(start)
 				case "latency_human":
-					return w.Write([]byte(stop.Sub(start).String()))
+					fields.LatencyHuman = stop.Sub(start).String()
 				case "bytes_in":
-					b := req.Header.Get(echo.HeaderContentLength)
-					if b == "" {
-						b = "0"
+					cl := req.Header.Get(echo.HeaderContentLength)
+					if cl == "" {
+						cl = "0"
 					}
-					return w.Write([]byte(b))
+					l, _ := strconv.ParseInt(cl, 10, 64)
+					fields.BytesIn = l
 				case "bytes_out":
-					return w.Write([]byte(strconv.FormatInt(res.Size, 10)))
+					fields.BytesOut = res.Size
 				default:
 					switch {
-					case strings.HasPrefix(tag, "header:"):
-						return buf.Write([]byte(c.Request().Header.Get(tag[7:])))
-					case strings.HasPrefix(tag, "query:"):
-						return buf.Write([]byte(c.QueryParam(tag[6:])))
-					case strings.HasPrefix(tag, "form:"):
-						return buf.Write([]byte(c.FormValue(tag[5:])))
+					case strings.HasPrefix(f, "header:"):
+						k := f[7:]
+						fields.Header[k] = c.Request().Header.Get(k)
+					case strings.HasPrefix(f, "form:"):
+						k := f[5:]
+						fields.Form[k] = c.Request().Header.Get(k)
+					case strings.HasPrefix(f, "query:"):
+						k := f[6:]
+						fields.Query[k] = c.Request().Header.Get(k)
 					}
 				}
-				return 0, nil
-			})
-			if err == nil {
-				config.Output.Write(buf.Bytes())
 			}
-			return
+
+			// Write
+			enc := json.NewEncoder(config.Output)
+			return enc.Encode(fields)
 		}
 	}
 }
