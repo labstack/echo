7dfec7e6419a1d249d4bda050a422ea3649defb3
Vishal Rana <vr@labstack.com>
2017-08-31 09:18:42 -0700

    Custom jwt errors (#999)
    * Custom error for jwt
    * New field `inner` in HTTPError to store error from external dependency
    
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"reflect"
@@ -55,6 +54,11 @@ type (
 // Algorithms
 const (
 	AlgorithmHS256 = "HS256"
+)
+
+// Errors
+var (
+	ErrJWTInvalid = echo.NewHTTPError(http.StatusBadRequest, "Missing or invalid jwt")
 )
 
 var (
@@ -134,7 +138,7 @@ func JWTWithConfig(config JWTConfig) echo.MiddlewareFunc {
 
 			auth, err := extractor(c)
 			if err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+				return err
 			}
 			token := new(jwt.Token)
 			// Issue #647, #656
@@ -150,7 +154,9 @@ func JWTWithConfig(config JWTConfig) echo.MiddlewareFunc {
 				c.Set(config.ContextKey, token)
 				return next(c)
 			}
-			return echo.ErrUnauthorized
+			he := echo.NewHTTPError(http.StatusUnauthorized, "Invalid or expired jwt")
+			he.Inner = err
+			return he
 		}
 	}
 }
@@ -163,7 +169,7 @@ func jwtFromHeader(header string, authScheme string) jwtExtractor {
 		if len(auth) > l+1 && auth[:l] == authScheme {
 			return auth[l+1:], nil
 		}
-		return "", errors.New("Missing or invalid jwt in the request header")
+		return "", ErrJWTInvalid
 	}
 }
 
@@ -172,7 +178,7 @@ func jwtFromQuery(param string) jwtExtractor {
 	return func(c echo.Context) (string, error) {
 		token := c.QueryParam(param)
 		if token == "" {
-			return "", errors.New("Missing jwt in the query string")
+			return "", ErrJWTInvalid
 		}
 		return token, nil
 	}
@@ -183,7 +189,7 @@ func jwtFromCookie(name string) jwtExtractor {
 	return func(c echo.Context) (string, error) {
 		cookie, err := c.Cookie(name)
 		if err != nil {
-			return "", errors.New("Missing jwt in the cookie")
+			return "", ErrJWTInvalid
 		}
 		return cookie.Value, nil
 	}
