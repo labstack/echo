9ad9290cd72d58e4df1c46e2c5d1ceec6750323a
Andy Zhang <andizzle.zhang@gmail.com>
2017-11-06 21:55:24 +1100

    add in Tracer interface and the DefaultTracer struct

--- stacktrace.go
+++ stacktrace.go
@@ -1,1 +1,14 @@
 package echo
+
+type (
+	// Tracer interface allows custom traceback
+	// function to be implemented
+	Tracer interface {
+		Trace(c Context)
+	}
+
+	// DefaultTracer offers basic traceback
+	DefaultTracer struct {
+		Full bool
+	}
+)
