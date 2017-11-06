31610438878605e1247f57ce5e7a9349a91f7a18
Andy Zhang <andizzle.zhang@gmail.com>
2017-11-06 21:55:55 +1100

    implement the trace functions to set the traceback to context

--- stacktrace.go
+++ stacktrace.go
@@ -1,5 +1,13 @@
 package echo
 
+import (
+	"runtime"
+	"strings"
+)
+
+// TraceContextKey is the key sets to context
+const TraceContextKey = "echo_context_traceback"
+
 type (
 	// Tracer interface allows custom traceback
 	// function to be implemented
@@ -12,3 +20,25 @@ type (
 		Full bool
 	}
 )
+
+// Trace run the stack trace
+func (t *DefaultTracer) Trace(c Context) {
+	rawStack := []byte{}
+	runtime.Stack(rawStack, t.Full)
+
+	stack := t.Format(rawStack)
+	c.Set(TraceContextKey, stack)
+}
+
+// Format formats the traceback result for easier processing
+func (t *DefaultTracer) Format(b []byte) []string {
+	stack := strings.Split(string(b), "\n\t")
+
+	for i, v := range stack {
+		v = strings.Trim(v, "\n")
+		v = strings.Replace(v, "\n", ": ", -1)
+		stack[i] = v
+	}
+
+	return stack
+}
