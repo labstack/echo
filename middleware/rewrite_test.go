ee32e3e7fb8aa5523483bce81d0c615c6cd27f66
Jeffrey Reuling (TI) <jreuling@to-increase.com>
2018-03-16 15:05:22 +0100

    fix issue #1086

--- middleware/rewrite_test.go
+++ middleware/rewrite_test.go
@@ -33,3 +33,28 @@ func TestRewrite(t *testing.T) {
 	e.ServeHTTP(rec, req)
 	assert.Equal(t, "/user/jack/order/1", req.URL.Path)
 }
+
+//Issue #1086
+func TestEchoRewritePreMiddleware(t *testing.T) {
+	e := echo.New()
+	r := e.Router()
+
+	// Rewrite old url to new one
+	e.Pre(RewriteWithConfig(RewriteConfig{
+		Rules: map[string]string{
+			"/old": "/new",
+		},
+	}))
+
+	// Route
+	r.Add(echo.GET, "/new", func(c echo.Context) error {
+		return c.NoContent(200)
+		return nil
+	})
+
+	req := httptest.NewRequest(echo.GET, "/old", nil)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(rec, req)
+	assert.Equal(t, "/new", req.URL.Path)
+	assert.Equal(t, 200, rec.Code)
+}
