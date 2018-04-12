7c3df7703f2f573138f10eeb2d03ab8136411a32
Vishal Rana <vr@labstack.com>
2018-04-11 22:34:01 -0700

    Revert "Allow parameter routes to end with a dot (/foo/:id.json)"
    This reverts commit bfa14633f3e679c24a0bc647a9445a1237127f17.

--- router_test.go
+++ router_test.go
@@ -784,9 +784,6 @@ func TestRouterParamNames(t *testing.T) {
 	r.Add(GET, "/users/:id", func(c Context) error {
 		return nil
 	})
-	r.Add(GET, "/companies/:id.json", func(c Context) error {
-		return nil
-	})
 	r.Add(GET, "/users/:uid/files/:fid", func(c Context) error {
 		return nil
 	})
@@ -799,11 +796,6 @@ func TestRouterParamNames(t *testing.T) {
 
 	// Route > /users/:id
 	r.Find(GET, "/users/1", c)
-	assert.Equal(t, "id", c.pnames[0])
-	assert.Equal(t, "1", c.Param("id"))
-
-	// Route -> /companies/:id.json
-	r.Find(GET, "/companies/1.json", c)
 	assert.Equal(t, "id", c.pnames[0])
 	assert.Equal(t, "1", c.Param("id"))
 
