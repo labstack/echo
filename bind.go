1fe59c577cb36abee5918b9598fc5fd4c77c7e20
Brandon Hansen <brandon@teamwork.com>
2018-05-30 15:09:39 -0700

    case sensitive matching prioritized

--- bind.go
+++ bind.go
@@ -104,18 +104,19 @@ func (b *DefaultBinder) bindData(ptr interface{}, data map[string][]string, tag 
 			}
 		}
 
-		var inputValue []string
-		var exists bool
-
-		// Go json.Unmarshal supports case insensitive binding.  However the url
-		// params are bound case sensitive which is inconsistent.  To fix this
-		// we must check all of the map values in a case-insensitive search.
-		inputFieldName = strings.ToLower(inputFieldName)
-		for k, v := range data {
-			if strings.ToLower(k) == inputFieldName {
-				inputValue = v
-				exists = true
-				break
+		inputValue, exists := data[inputFieldName]
+		if !exists {
+			// Go json.Unmarshal supports case insensitive binding.  However the
+			// url params are bound case sensitive which is inconsistent.  To
+			// fix this we must check all of the map values in a
+			// case-insensitive search.
+			inputFieldName = strings.ToLower(inputFieldName)
+			for k, v := range data {
+				if strings.ToLower(k) == inputFieldName {
+					inputValue = v
+					exists = true
+					break
+				}
 			}
 		}
 
