2641f1de62ecb40f545f68c098b46c444ac3173d
番茄 <cm@isspu.com>
2018-07-17 11:07:31 +0800

    add form log test

--- middleware/logger_test.go
+++ middleware/logger_test.go
@@ -93,7 +93,7 @@ func TestLoggerTemplate(t *testing.T) {
 			`"method":"${method}","uri":"${uri}","status":${status}, "latency":${latency},` +
 			`"latency_human":"${latency_human}","bytes_in":${bytes_in}, "path":"${path}", "referer":"${referer}",` +
 			`"bytes_out":${bytes_out},"ch":"${header:X-Custom-Header}", "protocol":"${protocol}"` +
-			`"us":"${query:username}", "cf":"${form:username}", "session":"${cookie:session}"}` + "\n",
+			`"us":"${query:username}", "cf":"${form:username}", "form":"${form}", "session":"${cookie:session}"}` + "\n",
 		Output: buf,
 	}))
 
@@ -150,7 +150,7 @@ func TestLoggerCustomTimestamp(t *testing.T) {
 			`"method":"${method}","uri":"${uri}","status":${status}, "latency":${latency},` +
 			`"latency_human":"${latency_human}","bytes_in":${bytes_in}, "path":"${path}", "referer":"${referer}",` +
 			`"bytes_out":${bytes_out},"ch":"${header:X-Custom-Header}",` +
-			`"us":"${query:username}", "cf":"${form:username}", "session":"${cookie:session}"}` + "\n",
+			`"us":"${query:username}", "cf":"${form:username}", "form":"${form}", "session":"${cookie:session}"}` + "\n",
 		CustomTimeFormat: customTimeFormat,
 		Output:           buf,
 	}))
