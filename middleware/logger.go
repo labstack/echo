3f8b45c8d0dd840198a6b24b3393e5ec553dcb85
skrassiev <bg2128506@gmail.com>
2018-12-05 08:13:48 -0800

    balance double-quotes in json (#1232)

--- middleware/logger.go
+++ middleware/logger.go
@@ -70,7 +70,7 @@ var (
 		Skipper: DefaultSkipper,
 		Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}",` +
 			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
-			`"status":${status},"error":"${error}","latency":${latency}","latency_human":"${latency_human}"` +
+			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"` +
 			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n",
 		CustomTimeFormat: "2006-01-02 15:04:05.00000",
 		Output:           os.Stdout,
