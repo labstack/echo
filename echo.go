d0e1723f03e36a0c831997d8b66ffccb1b959773
Alex Ungur <alexaandru@gmail.com>
2019-01-27 00:39:45 +0200

    Bumped up version to 3.3.9 (related to #1244)

--- echo.go
+++ echo.go
@@ -167,7 +167,7 @@ const (
 const (
 	charsetUTF8 = "charset=UTF-8"
 	// PROPFIND Method can be used on collection and property resources.
-	PROPFIND    = "PROPFIND"
+	PROPFIND = "PROPFIND"
 )
 
 // Headers
@@ -221,7 +221,7 @@ const (
 
 const (
 	// Version of Echo
-	Version = "3.3.dev"
+	Version = "3.3.9"
 	website = "https://echo.labstack.com"
 	// http://patorjk.com/software/taag/#p=display&f=Small%20Slant&t=Echo
 	banner = `
