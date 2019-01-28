0b714f34823a2ecfb0d72fcce647ee695eb83f5e
Alex Ungur <alexaandru@gmail.com>
2019-01-28 17:43:48 +0200

    Bumped up the version

--- echo.go
+++ echo.go
@@ -221,7 +221,7 @@ const (
 
 const (
 	// Version of Echo
-	Version = "3.3.10-dev"
+	Version = "3.3.11-dev"
 	website = "https://echo.labstack.com"
 	// http://patorjk.com/software/taag/#p=display&f=Small%20Slant&t=Echo
 	banner = `
