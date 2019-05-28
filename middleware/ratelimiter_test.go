2dee87f67921b0b86a8a7c35f0258670703bfe1e
Alper Hankendi <alper.hankendi@hepsiburada.com>
2019-05-28 22:59:18 +0300

    rate limiter as middleware

---
+++ middleware/ratelimiter_test.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+
+)
+
+func TestRateLimiter(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	rateLimit := RateLimiter()
+
+	h := rateLimit(func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	})
+
+	// g
+	h(c)
+	assert.Contains(t, rec.Header().Get("X-Ratelimit-Limit"), "99")
+
+	//
+	req = httptest.NewRequest(http.MethodPost, "/", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	assert.Error(t, h(c))
+
+
+}
+
+
