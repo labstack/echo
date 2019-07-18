af34c36bf532dd8a923d9ec44fa4c703c75c6582
Nattawit Chaiworawitsakul <c.nattawit@gmail.com>
2019-07-18 10:43:37 +0700

    remove nil field declaration

--- middleware/logger.go
+++ middleware/logger.go
@@ -73,7 +73,6 @@ var (
 			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"` +
 			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n",
 		CustomTimeFormat: "2006-01-02 15:04:05.00000",
-		Output:           nil,
 		colorer:          color.New(),
 	}
 )
