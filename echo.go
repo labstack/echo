389383b2c301b870779d2f79a87c5be509ebade1
Vishal Rana <vr@labstack.com>
2019-08-11 12:55:02 -0700

    Bumped vv4.1.9
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- echo.go
+++ echo.go
@@ -227,7 +227,7 @@ const (
 
 const (
 	// Version of Echo
-	Version = "4.1.8"
+	Version = "4.1.9"
 	website = "https://echo.labstack.com"
 	// http://patorjk.com/software/taag/#p=display&f=Small%20Slant&t=Echo
 	banner = `
