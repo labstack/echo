c30229036e8332ab195795927374673b40bac5b3
toimtoimtoim <desinformatsioon@gmail.com>
2021-08-22 19:47:59 +0300

    Fix example

--- middleware/timeout.go
+++ middleware/timeout.go
@@ -22,7 +22,7 @@ import (
 //	return func(next echo.HandlerFunc) echo.HandlerFunc {
 //		return func(c echo.Context) error {
 //			timeoutCtx, cancel := context.WithTimeout(c.Request().Context(), timeout)
-//			c.Request().WithContext(timeoutCtx)
+//			c.SetRequest(c.Request().WithContext(timeoutCtx))
 //			defer cancel()
 //			return next(c)
 //		}
