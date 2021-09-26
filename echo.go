c6f0c667f145b5e5347ba812c9de5a5a4280bac5
Martti T <desinformatsioon@gmail.com>
2021-09-26 18:56:43 +0300

    Update version and changelog for 4.6.1 (#1995)

--- echo.go
+++ echo.go
@@ -241,7 +241,7 @@ const (
 
 const (
 	// Version of Echo
-	Version = "4.6.0"
+	Version = "4.6.1"
 	website = "https://echo.labstack.com"
 	// http://patorjk.com/software/taag/#p=display&f=Small%20Slant&t=Echo
 	banner = `
