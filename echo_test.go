6cb3b7c046b9fe73f0a1341cd2eea8071a60cc04
eric <65116642+nonbutAworker@users.noreply.github.com>
2022-02-23 15:22:20 +0800

    remove redundant 0 in make chan (#2101)
    * remove 0 in make(chan,0) to fix go-staticcheck problem

--- echo_test.go
+++ echo_test.go
@@ -961,7 +961,7 @@ func TestEchoStartTLSByteString(t *testing.T) {
 			e := New()
 			e.HideBanner = true
 
-			errChan := make(chan error, 0)
+			errChan := make(chan error)
 
 			go func() {
 				errChan <- e.StartTLS(":0", test.cert, test.key)
@@ -999,7 +999,7 @@ func TestEcho_StartAutoTLS(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			e := New()
-			errChan := make(chan error, 0)
+			errChan := make(chan error)
 
 			go func() {
 				errChan <- e.StartAutoTLS(tc.addr)
