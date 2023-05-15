ef3bbb91f7963c80f87d77533a1a346433aa0ef6
Artyom Misko <nospam@misko.su>
2023-05-16 00:00:36 +0300

    Add middleware with support proxy headers

---
+++ middleware/proxy_headers.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	protoRegex = regexp.MustCompile(`(?i)(?:proto=)(https|http)`)
+)
+
+func ProxyHeaders() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if fwd := c.RealIP(); fwd != "" {
+				c.Request().RemoteAddr = fwd
+			}
+
+			if scheme := getScheme(c); scheme != "" {
+				c.Request().URL.Scheme = scheme
+			}
+
+			if c.Request().Header.Get(echo.HeaderXForwardedHost) != "" {
+				c.Request().Host = c.Request().Header.Get(echo.HeaderXForwardedHost)
+			}
+
+			if prefix := c.Request().Header.Get(echo.HeaderXForwardedPrefix); prefix != "" {
+				c.Request().RequestURI, _ = url.JoinPath(prefix, c.Request().RequestURI)
+				c.Request().URL.Path, _ = url.JoinPath(prefix, c.Request().URL.Path)
+			}
+			return next(c)
+		}
+	}
+}
+
+func getScheme(c echo.Context) string {
+	var scheme string
+
+	if proto := c.Request().Header.Get(echo.HeaderXForwardedProto); proto != "" {
+		scheme = strings.ToLower(proto)
+	} else if proto = c.Request().Header.Get(echo.HeaderForwarded); proto != "" {
+		if match := protoRegex.FindStringSubmatch(proto); len(match) > 1 {
+			scheme = strings.ToLower(match[1])
+		}
+	}
+	return scheme
+}
