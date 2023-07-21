4585ec222efde07a2c7a97fb1ad6f0a9b7aef3f4
Trim21 <trim21.me@gmail.com>
2023-07-22 07:56:50 +0800

    Update util.go

--- middleware/util.go
+++ middleware/util.go
@@ -57,6 +57,7 @@ func matchSubdomain(domain, pattern string) bool {
 	return false
 }
 
+// https://tip.golang.org/doc/go1.19#:~:text=Read%20no%20longer%20buffers%20random%20data%20obtained%20from%20the%20operating%20system%20between%20calls
 var randomReaderPool = sync.Pool{New: func() any {
 	return bufio.NewReader(rand.Reader)
 }}
