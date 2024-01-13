3d67098841eb91f11a2f578218045103d51e79ce
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2024-01-13 15:32:22 -0800

    Added Allow Content Type middleware and tests

---
+++ middleware/allow_content_type.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+func AllowContentType(contentTypes ...string) echo.MiddlewareFunc {
+	if len(contentTypes) == 0 {
+		panic("echo: allow-content middleware requires at least one content type")
+	}
+	allowedContentTypes := make(map[string]struct{}, len(contentTypes))
+	for _, ctype := range contentTypes {
+		allowedContentTypes[strings.TrimSpace(strings.ToLower(ctype))] = struct{}{}
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if c.Request().ContentLength == 0 {
+				// skip check for empty content body
+				return next(c)
+			}
+			s := strings.ToLower(strings.TrimSpace(c.Request().Header.Get("Content-Type")))
+			if i := strings.Index(s, ";"); i > -1 {
+				s = s[0:i]
+			}
+			if _, ok := allowedContentTypes[s]; ok {
+				return next(c)
+			}
+			return echo.NewHTTPError(http.StatusUnsupportedMediaType)
+		}
+	}
+}
---
+++ middleware/allow_content_type_test.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAllowContentType(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	res := httptest.NewRecorder()
+	c := e.NewContext(req, res)
+
+	h := AllowContentType("application/json", "text/plain")(func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	})
+
+	// Test valid content type
+	req.Header.Add("Content-Type", "application/json")
+	assert.NoError(t, h(c))
+
+	// Test invalid content type
+	req.Header.Add("Content-Type", "application/json")
+	assert.NoError(t, h(c))
+}
