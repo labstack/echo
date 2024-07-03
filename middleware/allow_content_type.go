357f8709dc25b7f8db1ca4d1e06d310e8e752d4d
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2024-07-02 19:37:30 -0700

    added setting of accept header in response

--- middleware/allow_content_type.go
+++ middleware/allow_content_type.go
@@ -11,6 +11,16 @@ import (
 func AllowContentType(contentTypes ...string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			var acceptTypes = ""
+			for i, contentType := range contentTypes {
+				acceptTypes += contentType
+
+				if i != len(contentTypes)-1 {
+					acceptTypes += ","
+				}
+			}
+			c.Response().Header().Set("Accept", acceptTypes)
+
 			mediaType, _, err := mime.ParseMediaType(c.Request().Header.Get("Content-Type"))
 			if err != nil {
 				return echo.NewHTTPError(http.StatusBadRequest, "invalid content-type value")
