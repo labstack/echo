682dfde74c313efdb41c843a889eae6b55c7f204
kolaente <k@knt.li>
2025-01-24 12:27:09 +0100

    fix: check array values

--- bind.go
+++ bind.go
@@ -232,7 +232,12 @@ func (b *DefaultBinder) bindData(destination interface{}, data map[string][]stri
 			}
 		}
 
-		inputValue, exists := data[inputFieldName]
+		var inputValue []string
+		var exists bool
+		inputValue, exists = data[inputFieldName]
+		if !exists {
+			inputValue, exists = data[inputFieldName+"[]"]
+		}
 		if !exists {
 			// Go json.Unmarshal supports case-insensitive binding.  However the
 			// url params are bound case-sensitive which is inconsistent.  To
