482bb46fe5c7eb7c9fd7bec7d3128433dea21bee
toimtoimtoim <desinformatsioon@gmail.com>
2026-01-01 12:52:26 +0200

    v4.15.0 changelog

--- echo.go
+++ echo.go
@@ -267,7 +267,7 @@ const (
 
 const (
 	// Version of Echo
-	Version = "4.14.0"
+	Version = "4.15.0"
 	website = "https://echo.labstack.com"
 	// http://patorjk.com/software/taag/#p=display&f=Small%20Slant&t=Echo
 	banner = `
