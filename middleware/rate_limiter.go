e38e2348614352ddbf1426b09ebef6a37ba74938
cgalibern <cgalibern@users.noreply.github.com>
2026-01-06 20:03:34 +0100

    docs: add missing err parameter to DenyHandler example

--- middleware/rate_limiter.go
+++ middleware/rate_limiter.go
@@ -100,7 +100,7 @@ RateLimiterWithConfig returns a rate limiting middleware
 		ErrorHandler: func(context echo.Context, err error) error {
 			return context.JSON(http.StatusTooManyRequests, nil)
 		},
-		DenyHandler: func(context echo.Context, identifier string) error {
+		DenyHandler: func(context echo.Context, identifier string, err error) error {
 			return context.JSON(http.StatusForbidden, nil)
 		},
 	}
