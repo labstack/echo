c6b9f6affa1b2dc19555bb88c3676136fe8de403
Cyril Galibern <cyril.galibern@opensvc.com>
2026-01-26 16:25:23 +0100

    Update RateLimiterWithConfig example

--- middleware/rate_limiter.go
+++ middleware/rate_limiter.go
@@ -89,10 +89,10 @@ RateLimiterWithConfig returns a rate limiting middleware
 			id := ctx.RealIP()
 			return id, nil
 		},
-		ErrorHandler: func(context *echo.Context, err error) error {
+		ErrorHandler: func(ctx *echo.Context, err error) error {
 			return context.JSON(http.StatusTooManyRequests, nil)
 		},
-		DenyHandler: func(context *echo.Context, identifier string) error {
+		DenyHandler: func(ctx *echo.Context, identifier string, err error) error {
 			return context.JSON(http.StatusForbidden, nil)
 		},
 	}
